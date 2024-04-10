8d08886a0f68b0c079197fc0d6c32a5f98b83bef
TheLadyJ <jovana.vida@gmail.com>
2024-04-10 16:39:16 +0200

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class HomePage {
+  private movieService = inject(MovieService);
+  constructor() {
+    this.loadMovies();
+  }
+
+  loadMovies(){
+    this.movieService.getTopRatedMovies().subscribe((movies)=>{
+      console.log(movies)
+    })
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiKey: "8baba8ab6b8bbe247645bcae7df63d0d"
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
